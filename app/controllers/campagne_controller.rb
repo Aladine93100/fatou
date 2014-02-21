0e4927ed463441575127d7379f140b5d91679d1e
Aladine <alazaiane@yahoo.fr>
2014-02-21 11:57:27 +0100

    push heroku

---
+++ app/controllers/campagne_controller.rb
@@ -0,0 +1,10 @@
+class CampagneController < ApplicationController
+	def programme
+	end
+
+	def parole
+	end
+
+	def presentation
+	end
+end
---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+FatouApp::Application.initialize!
